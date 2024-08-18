df6d04e7e588f7699dfedbf851ca7d70c1a8c8fb
kingkiboots <tuffhr4@gmail.com>
2024-08-19 00:18:15 +0900

    Feat: Set Layout Center

--- FrontEnd/src/pages/siblingRivalry/ui/SiblingRivalryPage.tsx
+++ FrontEnd/src/pages/siblingRivalry/ui/SiblingRivalryPage.tsx
@@ -3,18 +3,25 @@ import {
   SolveWidget,
   UserInputsWidget,
 } from "@/widgets/siblingRivalry";
-import { Box, VStack } from "@chakra-ui/react";
+import { Box, Center, VStack } from "@chakra-ui/react";
 
 export const SiblingRivalryPage = () => {
   return (
-    <Box height="100vh">
-      <VStack spacing={3} align="stretch">
-        <UserInputsWidget />
-        <Box display="flex" justifyContent="center" alignItems="center" gap={3}>
-          <MainGameBoardWidget />
-        </Box>
-        <SolveWidget />
-      </VStack>
+    <Box height="100vh" width="100wh" overflow="scroll">
+      <Center>
+        <VStack spacing={3} align="stretch" maxWidth="667px">
+          <UserInputsWidget />
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            gap={3}
+          >
+            <MainGameBoardWidget />
+          </Box>
+          <SolveWidget />
+        </VStack>
+      </Center>
     </Box>
   );
 };
