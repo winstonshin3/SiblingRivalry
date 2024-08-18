bd52b5fb8c769c4b410a363c57caaa97d43f0e04
kingkiboots <tuffhr4@gmail.com>
2024-08-18 22:19:40 +0900

    Feat: Add Chakra Provider

--- FrontEnd/src/app/ui/AppEntry.tsx
+++ FrontEnd/src/app/ui/AppEntry.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { SiblingRivalryPage } from "@/pages/siblingRivalry";
+import { ChakraProvider } from "@chakra-ui/react";
+
+/**
+ * {1} : TODO: change ChakraProvider to ChakraBaseProvider for JS payload https://v2.chakra-ui.com/getting-started#chakrabaseprovider
+ */
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SiblingRivalryPage />
+    {/* {1} */}
+    <ChakraProvider>
+      <SiblingRivalryPage />
+    </ChakraProvider>
   </StrictMode>
 );
