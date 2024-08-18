6fa6dd8248e6b9d1ed5f28c0035c73d37796f4e1
kingkiboots <tuffhr4@gmail.com>
2024-08-19 00:02:23 +0900

    Feat: Combine Added UI Widgets

--- FrontEnd/src/pages/siblingRivalry/ui/SiblingRivalryPage.tsx
+++ FrontEnd/src/pages/siblingRivalry/ui/SiblingRivalryPage.tsx
@@ -1,11 +1,19 @@
-import { UserInputsWidget } from "@/widgets/siblingRivalry";
+import {
+  MainGameBoardWidget,
+  SolveWidget,
+  UserInputsWidget,
+} from "@/widgets/siblingRivalry";
 import { Box, VStack } from "@chakra-ui/react";
 
 export const SiblingRivalryPage = () => {
   return (
     <Box height="100vh">
-      <VStack spacing={4} align="stretch">
+      <VStack spacing={3} align="stretch">
         <UserInputsWidget />
+        <Box display="flex" justifyContent="center" alignItems="center" gap={3}>
+          <MainGameBoardWidget />
+        </Box>
+        <SolveWidget />
       </VStack>
     </Box>
   );
