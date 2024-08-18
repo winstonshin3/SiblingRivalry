de7d11230a71f62944a0181fe4363f65afcefa36
kingkiboots <tuffhr4@gmail.com>
2024-08-18 22:16:48 +0900

    Remove: Remove initially provided default css style

--- FrontEnd/src/app/ui/AppEntry.tsx
+++ FrontEnd/src/app/ui/AppEntry.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "../styles/index.css";
 import { SiblingRivalryPage } from "@/pages/siblingRivalry";
 
 createRoot(document.getElementById("root")!).render(
