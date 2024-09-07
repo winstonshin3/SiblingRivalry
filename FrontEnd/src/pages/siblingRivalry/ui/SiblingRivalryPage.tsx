812d5b9572cc09a9999280a2558b36907f04df32
kingkiboots <tuffhr4@gmail.com>
2024-09-08 01:12:40 +0900

    Feat: Add Background Image and BackgroundColor

--- FrontEnd/src/pages/siblingRivalry/ui/SiblingRivalryPage.tsx
+++ FrontEnd/src/pages/siblingRivalry/ui/SiblingRivalryPage.tsx
@@ -7,9 +7,38 @@ import { Box, Center, VStack } from "@chakra-ui/react";
 
 export const SiblingRivalryPage = () => {
   return (
-    <Box height="100vh" width="100wh">
-      <Center>
-        <VStack spacing={3} align="stretch" maxWidth="667px" overflow="auto">
+    <Box
+      height="100vh"
+      width="100wh"
+      backgroundImage={
+        "url('https://static.wikia.nocookie.net/necromerger/images/0/08/Site-background-dark')"
+      }
+      color={"white"}
+      backgroundSize={"310px auto"}
+      backgroundAttachment={"fixed"}
+      style={{ imageRendering: "pixelated" }}
+    >
+      <Center height="100vh">
+        <VStack
+          backgroundColor="#1e0c1b"
+          className="main_page"
+          minHeight={"480px"}
+          // TODO: 스타일은 나중에 먹이기
+          // style={{
+          //   borderImageOutset: "20px 20px 5px 20px",
+          //   borderRadius: "3px 0 0 3px",
+          //   borderImageSlice: "20 fill",
+          //   borderImageWidth: "40px",
+          //   borderImageSource:
+          //     'url("https://static.wikia.nocookie.net/necromerger/images/e/e2/PanelBacking_Dark.png")',
+          // }}
+          spacing={3}
+          align="stretch"
+          maxWidth="667px"
+          overflow="auto"
+          padding={"24px 36px"}
+          as="main"
+        >
           <UserInputsWidget />
           <Box>
             <MainGameBoardWidget />
